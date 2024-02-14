df9c33c31f65cc9d51eee3caf09b9993ab6bca3e
Charlie <charliehu2808@outlook.com>
2024-02-14 17:18:19 +0100

    satisfied

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(s, num) {
+    if (num >= 0) {
+        let str = "";
+        for (let i = 0; i < num; i++) {
+            str += s;
+        }
+        return str;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
